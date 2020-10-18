aa398903e8cbf7e9515d17553fe6df4345068b2f
Akash Singha <40393038+tensor1995@users.noreply.github.com>
2020-10-19 01:52:47 +0530

    Add files via upload

---
+++ scraper-schema.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const scraperSchema = new Schema({
+    url: {
+        type: String,
+        unique: true
+    },
+    totalRef: Number,
+    params: String
+});
